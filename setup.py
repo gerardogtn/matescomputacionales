3a4f1aa4a6b163e4f2f19cbce9378cce6ecf1b99
Gerardo Teruel <gerardogtn@gmail.com>
2016-06-01 11:29:18 -0500

    :octocat: First commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Proyectos de matematicas computacionales',
+    'author': 'Gerardo Teruel',
+    'url': 'https://github.com/gerardogtn',
+    'download_url': 'https://github.com/gerardogtn/matematicascomputacionales',
+    'author_email': 'gerardogtn@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['projects'],
+    'scripts': [],
+    'name': 'Projects'
+}
+
+setup(**config)
