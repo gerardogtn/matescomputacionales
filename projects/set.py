e303d72fc944377b86178b3ab6e105ac374cbc6b
Gerardo Teruel <gerardogtn@gmail.com>
2016-06-01 15:21:55 -0500

    :rocket: Add simple set class

---
+++ projects/set.py
@@ -0,0 +1,30 @@
+class Set:
+    entries = []
+
+    def __init__(self):
+        pass
+
+    def add(self, entry):
+        if (entry not in self.entries):
+            self.entries.append(entry)
+
+    def addAll(self, entries):
+        for e in entries:
+            self.add(e)
+
+    def remove(self, entry):
+        if (entry in self.entries):
+            self.entries.remove(entry)
+
+    def removeAll(self, entries):
+        for e in entries:
+            remove(e)
+
+    def contains(self, entry):
+        return entry in self.entries
+
+    def isEmpty(self):
+        return len(self.entries) == 0
+
+    def size(self):
+        return len(self.entries)
---
+++ tests/set_tests.py
@@ -0,0 +1,30 @@
+from nose.tools import *
+from projects.set import Set
+import projects
+
+def setup():
+    print "SETUP!"
+
+def teardown():
+    print "TEAR DOWN!"
+
+def test_add():
+    set = Set()
+    set.add(1)
+    assert set.contains(1)
+
+def test_add_duplicates():
+    set = Set()
+    set.addAll([1, 1, 3])
+    assert set.size() == 2
+    assert set.contains(1)
+    assert set.contains(3)
+
+def test_remove():
+    set = Set()
+    set.addAll([1, 3, 5])
+    assert set.size() == 3
+    assert set.contains(3)
+    set.remove(3)
+    assert set.size() == 2
+    assert (not set.contains(3))
