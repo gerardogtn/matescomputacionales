4aa1d68bbc7e9b1ad69fb2ca30040d3ea4c0dabc
Gerardo Teruel <gerardogtn@gmail.com>
2016-06-02 17:09:30 -0500

    :memo: Add docstrings

--- projects/relations.py
+++ projects/relations.py
@@ -1,7 +1,9 @@
 from set import Set
-## Given a list of pairs, representing a relation, return true if
-## the relation is reflexive, False otherwise.
+
 def isReflexive(pairs):
+    """ Given a list of pair(a list of two elements). Return true if
+    the pairs are reflexive, false otherwise.
+    """
     allSet = Set()
     reflexiveSet = Set()
     for p in pairs:
@@ -10,19 +12,19 @@ def isReflexive(pairs):
             reflexiveSet.add(p[0])
     return allSet == reflexiveSet
 
-## List<List<Char>> -> Bool
-## Given a list of pairs, representing a relation, return true if
-## the relation is irreflexive, False otherwise.
 def isIrreflexive(pairs):
+    """ Given a list of pair(a list of two elements). Return true if
+    the pairs are irreflexive, false otherwise.
+    """
     for p in pairs:
         if (p[0] == p[1]):
             return False
     return True
 
-## List<List<Char>> -> Bool
-## Given a list of pairs, representing a relation, return true if
-## the relation is transitive, False otherwise.
 def isTransitive(pairs):
+    """ Given a list of pair(a list of two elements). Return true if
+    the pairs are transitive, false otherwise.
+    """
     transitiveSet = Set()
     for p1 in pairs:
         for p2 in pairs:
@@ -37,6 +39,9 @@ def isTransitive(pairs):
 
 
 def containsAll(firstPair, secondPair, set):
+    """ Given two pairs (list of two entries) and a set, return True if
+    set contains all the elements in both pairs. False otherwise.
+    """
     out = True
     for e in firstPair:
         out = out and set.contains(e)
@@ -45,10 +50,10 @@ def containsAll(firstPair, secondPair, set):
     return out
 
 
-## List<List<Char>> -> Bool
-## Given a list of pairs, representing a relation, return true if
-## the relation is symmetric, False otherwise.
 def isSymmetric(pairs):
+    """ Given a list of pair(a list of two elements). Return true if
+    the pairs are symmetric, false otherwise.
+    """
     out = True
     for p1 in pairs:
         found = False
@@ -59,18 +64,18 @@ def isSymmetric(pairs):
         out = out and found
     return out
 
-## List<List<Char>> -> Bool
-## Given a list of pairs, representing a relation, return true if
-## the relation is asymetric, False otherwise.
 def isAsymmetric(pairs):
+    """ Given a list of pair(a list of two elements). Return true if
+    the pairs are asymmetric, false otherwise.
+    """
     for p1 in pairs:
         for p2 in pairs:
             if (p1[0] == p2[1] and p1[1] == p2[0]):
                 return False
     return True
 
-## List<List<Char>> -> Bool
-## Given a list of pairs, representing a relation, return true if
-## the relation is equivalent, False otherwise.
 def isEquivalent(pairs):
+    """ Given a list of pair(a list of two elements). Return true if
+    the pairs are equivalent, false otherwise.
+    """
     return isReflexive(pairs) and isSymmetric(pairs) and isTransitive(pairs)
