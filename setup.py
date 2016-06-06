d97dd07e40d84ac1a0e8fe6cdfc2e061118fe27d
Gerardo Teruel <gerardogtn@gmail.com>
2016-06-06 10:33:56 -0500

    :rocket: Migrate from nosetests to py.test

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'download_url': 'https://github.com/gerardogtn/matematicascomputacionales',
     'author_email': 'gerardogtn@gmail.com',
     'version': '0.1',
-    'install_requires': ['nose'],
+    'install_requires': ['py.test'],
     'packages': ['project01', 'datastructures'],
     'scripts': [],
     'name': 'Projects'
