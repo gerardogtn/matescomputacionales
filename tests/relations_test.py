6e82fa9bcd52e398aadfcd678c531f2e4483dfd9
Gerardo Teruel <gerardogtn@gmail.com>
2016-06-09 16:15:47 -0500

    :checkmark: Add test for incomplete symmetric relation

--- tests/relations_test.py
+++ tests/relations_test.py
@@ -30,6 +30,9 @@ def test_not_transitive():
 def test_symetric():
     assert r.isSymmetric([[1, 2], [2, 1], [3, 4], [4, 3], [1, 1]])
 
+def test_incomplete_symetric():
+    assert not r.isSymmetric([[1, 2], [2, 1], [3, 4], [4, 3], [3, 2]])
+
 def test_not_symetric():
     assert not r.isSymmetric([[1, 2], [2, 3]])
 
