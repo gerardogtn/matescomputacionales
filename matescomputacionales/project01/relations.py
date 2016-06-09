9a20b489cc1f6a3ce78beb89301e41bfef05b13d
Gerardo Teruel <gerardogtn@gmail.com>
2016-06-09 16:16:04 -0500

    :rocket: Add containsAll method

--- matescomputacionales/project01/relations.py
+++ matescomputacionales/project01/relations.py
@@ -30,37 +30,22 @@ def isTransitive(pairs):
         for p2 in pairs:
             if p1[1] == p2[0]:
                 transitivePairs.append([p1[0], p2[1]])
-    for transitivePair in transitivePairs:
-        if transitivePair not in pairs:
+    return containsAll(pairs, transitivePairs)
+
+def containsAll(pairs, targets):
+    for t in targets:
+        if t not in pairs:
             return False
     return True
-
-
-def containsAll(firstPair, secondPair, set):
-    """ Given two pairs (list of two entries) and a set, return True if
-    set contains all the elements in both pairs. False otherwise.
-    """
-    out = True
-    for e in firstPair:
-        out = out and set.contains(e)
-    for e in secondPair:
-        out = out and set.contains(e)
-    return out
-
 
 def isSymmetric(pairs):
     """ Given a list of pair(a list of two elements). Return true if
     the pairs are symmetric, false otherwise.
     """
-    out = True
-    for p1 in pairs:
-        found = False
-        for p2 in pairs:
-            if (p1[0] == p2[1] and p1[1] == p2[0]):
-                found = True
-                break
-        out = out and found
-    return out
+    symmetricPairs = []
+    for p in pairs:
+        symmetricPairs.append([p[1], p[0]])
+    return containsAll(pairs, symmetricPairs)
 
 def isAsymmetric(pairs):
     """ Given a list of pair(a list of two elements). Return true if
