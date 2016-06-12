abdbfa458b82b309492d4a0cd9227c99469eae04
Gerardo Teruel <gerardogtn@gmail.com>
2016-06-12 16:55:01 -0500

    :rocket: Change from iterative to recursive implementation and from custom set to built in python set

--- matescomputacionales/project02/recursive_definitions.py
+++ matescomputacionales/project02/recursive_definitions.py
@@ -1,9 +1,19 @@
-from matescomputacionales.datastructures.set import Set
-
 """ All strings used as patterns in a recursive definition"""
 patternStrings = ['u', 'v', 'w', 'x', 'y', 'z']
 
-def getStringsUntilNRecursiveStep(baseCases, recursiveSteps, N):
+def getAllCombinationsForStep(step, combinations, out):
+    # print 'Recursive call with step: ', step, " combinations: ", combinations, " out: ", out
+    patternsInStep = filter(lambda x: x in patternStrings, step)
+    if not patternsInStep :
+        out.add(step)
+        return out
+    else:
+        for c in combinations:
+            out.union(getAllCombinationsForStep(step.replace(patternsInStep[0], c), combinations, out))
+    return out
+
+
+def getStringsUntilNRecursiveStep(baseCases, recursiveSteps, N, callback=(lambda x: x)):
     """ Get all the strings formed until N recursive steps
 
     Keyword arguments:
@@ -15,19 +25,14 @@ def getStringsUntilNRecursiveStep(baseCases, recursiveSteps, N):
     """
 
     n = 0
-    set = Set(baseCases)
-    previous = baseCases
-    while n < N :
-        current = []
+    allStrings = set()
+    allStrings = allStrings.union(baseCases)
+    while n < N:
+        current = set()
         for step in recursiveSteps:
-            for p in previous:
-                newElement = step
-                for pattern in patternStrings:
-                    newElement = newElement.replace(pattern, p)
-                current.append(newElement)
-                set.add(newElement)
-
-        previous = current
+            current = current.union(getAllCombinationsForStep(step, allStrings, set()))
+        callback(current - current.intersection(allStrings))
+        allStrings = allStrings.union(current)
         n = n + 1
 
-    return set
+    return allStrings
--- tests/recursive_definitions_test.py
+++ tests/recursive_definitions_test.py
@@ -1,44 +1,56 @@
 import matescomputacionales.project02.recursive_definitions as r
-from matescomputacionales.datastructures.set import Set
 
 def test_empty_empty_at_zero():
-    actual = r.getStringsUntilNRecursiveStep([''], ['u'], 0)
-    expected = Set()
-    expected.add('')
+    actual = r.getStringsUntilNRecursiveStep({''}, {'u'}, 0)
+    expected = {""}
     print actual, expected
     assert actual == expected
 
 def test_empty_empty_at_ten():
-    actual = r.getStringsUntilNRecursiveStep([''], ['u'], 10)
-    expected = Set()
-    expected.add('')
+    actual = r.getStringsUntilNRecursiveStep({''}, {'u'}, 10)
+    expected = {""}
     print actual, expected
     assert actual == expected
 
 def test_empty_1_at_zero():
-    actual = r.getStringsUntilNRecursiveStep([''], ['u1'], 0)
-    expected = Set()
-    expected.add('')
+    actual = r.getStringsUntilNRecursiveStep({''}, {'u1'}, 0)
+    expected = {""}
     print actual, expected
     assert actual == expected
 
 def test_empty_1_at_three():
-    expected = Set()
-    expected.addAll(['', '1', '11', '111'])
-    actual = r.getStringsUntilNRecursiveStep([''], ['u1'], 3)
+    expected = {"", "1", "11", "111"}
+    actual = r.getStringsUntilNRecursiveStep({''}, {'u1'}, 3)
     print actual, expected
     assert actual == expected
 
+def test_empty_1_or_zero_at_1():
+    expected = {"", "1", "0"}
+    actual = r.getStringsUntilNRecursiveStep({''}, {'u1', 'u0'}, 1)
+    print actual, expected
+    assert actual == expected
+
+
 def test_empty_1_or_zero_at_2():
-    expected = Set()
-    expected.addAll(['', '1', '0', '11', '01', '00', '10'])
-    actual = r.getStringsUntilNRecursiveStep([''], ['u1', 'u0'], 2)
+    expected = {'', '1', '0', '11', '01', '00', '10'}
+    actual = r.getStringsUntilNRecursiveStep({''}, {'u1', 'u0'}, 2)
     print actual, expected
     assert actual == expected
 
 def test_1__1_or_zero_at_2():
-    expected = Set()
-    expected.addAll(['1', '11', '10', '111', '101', '100', '110'])
-    actual = r.getStringsUntilNRecursiveStep(['1'], ['u1', 'u0'], 2)
+    expected = {'1', '11', '10', '111', '101', '100', '110'}
+    actual = r.getStringsUntilNRecursiveStep({'1'}, {'u1', 'u0'}, 2)
+    print actual, expected
+    assert actual == expected
+
+def test_multiple_patterns_at_1():
+    expected = {'', '0', '1'}
+    actual = r.getStringsUntilNRecursiveStep({''}, {'uu0', 'w1u'}, 1)
+    print actual, expected
+    assert actual == expected
+
+def test_multiple_patterns_at_2():
+    expected = {'', '0', '1', '000', '110', '010', '111', '011', '01', '11', '10'}
+    actual = r.getStringsUntilNRecursiveStep({''}, {'uu0', 'w1u'}, 2)
     print actual, expected
     assert actual == expected
