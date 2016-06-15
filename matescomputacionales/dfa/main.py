bfab3b407ddbe8a5afabc4864b982deab13a495c
Gerardo Teruel <gerardogtn@gmail.com>
2016-06-15 10:28:25 -0500

    :rocket: Update file reading format

--- matescomputacionales/dfa/main.py
+++ matescomputacionales/dfa/main.py
@@ -7,18 +7,14 @@ finalStates = []
 transitionFunction = {}
 
 with open(sys.argv[1], 'r') as file:
-    states = file.readline().split()
     initialState = file.readline().split()[0]
     finalStates = file.readline().split()
 
-    n = 0
     for line in file:
         values = line.split()
         current = {}
-        for i in range(0, len(values), 2):
+        for i in range(1, len(values), 2):
             current[values[i]] = values[i + 1]
-        transitionFunction[states[n]] = current
-        n = n + 1
+        transitionFunction[values[0]] = current
 
-print transitionFunction
 print ("Belongs" if belongs(transitionFunction, initialState, finalStates, sys.argv[2]) else "DOESN'T belong")
